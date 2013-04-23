573445cf4174173687bf4ee49b91a56dae47c891
piglovesyou <thepiglovesyou@gmail.com>
2013-04-23 22:49:07 +0900

    yeah

--- src/schema/itembase.js
+++ src/schema/itembase.js
@@ -4,5 +4,12 @@
 module.exports = {
   type: String, // (youtube|twitter|...)
   like: {type: [ String ], default: [] },
-  bad: {type: [ String ], default: [] }
+  bad: {type: [ String ], default: [] },
+  comment: [
+    {
+      userId: String,
+      body: String,
+      updateAt: {type: Date, default: Date.now}
+    }
+  ]
 };
