08b1a9acdd7dad30a68ef7a7bec55a073ec17821
piglovesyou <thepiglovesyou@gmail.com>
2013-05-06 13:53:07 +0900

    add auth test (not enough)

---
+++ test/authenticate_test.js
@@ -0,0 +1,44 @@
+
+assert = require('assert');
+webdriver = require('selenium-webdriver');
+buildDriver = function() {
+  var d = new webdriver.Builder()
+    .usingServer('http://localhost:4444/wd/hub')
+    .withCapabilities({
+      browserName: 'firefox'
+    }).build();
+  d.manage().window().setSize(350, 200);
+  return d;
+};
+
+var whenFail = function(driver) {
+  return function(reason) {
+    driver.quit().then(function() {
+      throw new Error(reason);
+    });
+  }
+};
+
+
+
+describe('auth', function() {
+  this.timeout(30 * 1000);
+
+  it('#User should log in', function(done) {
+    var d = buildDriver();
+    d.get('http://nu-minor.com');
+
+    d.getTitle()
+    .then(function(title) {
+      assert.equal(title, 'Plovr Sample xx');
+    })
+    .then(function() {
+      return d.quit();
+    })
+    .then(null, whenFail(d))
+    .then(function() {
+      done();
+    });
+  });
+});
+
