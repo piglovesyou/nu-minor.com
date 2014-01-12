ee224cbe4b8d9a5291f8508b7ccbb01e2e86f0fa
piglovesyou <thepiglovesyou@gmail.com>
2014-01-12 20:41:26 +0900

    refac: all collector

--- src/collector/soundcloud.js
+++ src/collector/soundcloud.js
@@ -7,62 +7,52 @@ var CLIENT_ID = require('secret-strings').NU_MINOR.SOUNDCLOUD_CLIENT_ID;
 
 var update = Q.denodeify(db.item.update.bind(db.item));
 
-var deferredToExport = Q.defer();
+
 
 /** @type {Object} */
-module.exports.promise = deferredToExport.promise;
-
-// A local promise object where steps are chained in this file.
-var promiseCollectYoutube;
+module.exports.promise = promise;
+module.exports.get = get;
 
 
-var createUrl = function(path, param) {
+
+function promise() {
+  return Q.when('/users/nu-minor/tracks.json')
+  .then(get)
+  .then(function(res) {
+    return res.json;
+  })
+  .then(insertItems)
+  .fail(whenFail);
+}
+
+function get(path) {
+  return http.get(createUrl(path));
+};
+
+function whenFail(reason) {
+  throw new Error(reason);
+};
+
+function insertItems(items) {
+  return Q.allSettled(items.map(function(item, i) {
+    item.nm_type = 'soundcloud';
+    return insertItem(item);
+  }))
+  .then(function() {
+    return items.length;
+  })
+  .fail(whenFail);
+};
+
+function insertItem(item) {
+  return update({ id: item.id },
+    item, { upsert: true });
+};
+
+function createUrl(path, param) {
   if (!param) param = {};
   param.client_id = CLIENT_ID;
   return 'http://api.soundcloud.com' +
       path + '?' + querystring.stringify(param);
 };
 
-var get = function(path) {
-  return http.get(createUrl(path));
-};
-module.exports.get = get;
-
-var whenFail = function(reason) {
-  throw new Error(reason);
-};
-
-var whenAllDone = function(length) {
-  deferredToExport.resolve(length);
-};
-
-var insertItem = function(item) {
-  return update({ id: item.id },
-    item, { upsert: true });
-};
-
-var insertItems = function(items) {
-
-  var d = Q.defer();
-  Q.allSettled(items.map(function(item, i) {
-    item.nm_type = 'soundcloud';
-    return insertItem(item);
-  }))
-  .fail(whenFail)
-  .done(function() {
-    d.resolve(items.length);
-  });
-  return d.promise;
-};
-
-
-promiseCollectYoutube = Q.when()
-.then(function() {
-  return get('/users/nu-minor/tracks.json');
-})
-.then(function(res) {
-  return res.json;
-})
-.then(insertItems)
-.fail(whenFail)
-.done(whenAllDone);
