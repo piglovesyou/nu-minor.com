57b5d396ff54e5af73a5b6c73ac4ff7cef2d9a8d
piglovesyou <thepiglovesyou@gmail.com>
2014-01-17 01:00:27 +0900

    use masonry

---
+++ public/javascripts/m.js
@@ -0,0 +1,11 @@
+
+var m = new Masonry(document.querySelector('.app-items'), {
+  itemSelector: '.app-item',
+  isInitLayout: false
+});
+m._originalLayout = m.layout;
+m.layout = function() {
+  m.options.columnWidth = getSize(document.querySelector('.span3')).outerWidth;
+  m._originalLayout();
+};
+m.layout();
