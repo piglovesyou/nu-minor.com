5736c781eff8e3e35fe2ff288f1711817e1193f4
piglovesyou <thepiglovesyou@gmail.com>
2014-01-19 13:34:49 +0900

    mod all test: check existence, not totalCount

--- test/twiter_test.js
+++ test/twiter_test.js
@@ -12,18 +12,19 @@ describe('TwitterCollector', function() {
     var actualCount, twitter;
     actualCount = 0;
     twitter = require('../src/collector/twitter');
-    return twitter.promise().then(function() {
+    return twitter.promise()
+    .then(function() {
       return twitter.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=NU_minor');
     }).then(function(items) {
-      actualCount = items.length;
-      return find({
-        nm_type: 'twitter'
-      });
-    }).then(function(dbItems) {
-      assert(_.isArray(dbItems));
-      assert.equal(dbItems.length, actualCount);
-      return done();
-    }).done();
+      assert(_.isArray(items));
+      return items.reduce(function(p, item) {
+        return p.then(function() {
+          return count({id: item.id});
+        }).then(function(count) {
+          assert.equal(count, 1);
+        });
+      }, Q());
+    }).then(done);
   });
 });
 
--- test/youtube_test.js
+++ test/youtube_test.js
@@ -1,6 +1,7 @@
 var Q = require('q');
 var youtube = require('youtube-feeds');
 var assert = require('assert');
+var _ = require('underscore');
 var db = require('../src/setupdb');
 var count = Q.denodeify(db.item.count.bind(db.item));
 var videos = Q.denodeify(youtube.feeds.videos.bind(youtube.feeds));
@@ -12,19 +13,19 @@ describe('YoutubeCollector', function() {
     total = void 0;
     return require('../src/collector/youtube').promise().then(function() {
       return videos({
-        author: 'NUminormusic',
-        'max-results': 1
+        author: 'NUminormusic'
       });
     }).then(function(data) {
-      total = data.totalItems;
-      return count({
-        nm_type: 'youtube'
-      });
-    }).done(function(actual) {
-      console.log('total:', total, 'actual:', actual);
-      assert.equal(total, actual);
-      return done();
-    });
+      var items = data.items;
+      assert(_.isArray(items));
+      return items.reduce(function(p, item) {
+        return p.then(function() {
+          return count({id: item.id});
+        }).then(function(count) {
+          assert.equal(count, 1);
+        });
+      }, Q());
+    }).then(done);
   });
 });
 
