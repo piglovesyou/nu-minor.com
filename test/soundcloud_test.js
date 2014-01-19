b391e47d01072edef747378308bfbb27c6215e51
piglovesyou <thepiglovesyou@gmail.com>
2014-01-19 13:21:14 +0900

    mod test: check existance, not total.

--- test/soundcloud_test.js
+++ test/soundcloud_test.js
@@ -1,4 +1,5 @@
 var Q = require('q');
+var _ = require('underscore');
 var youtube = require('youtube-feeds');
 var assert = require('assert');
 var db = require('../src/setupdb');
@@ -11,18 +12,21 @@ describe('SoundCloudCollector', function() {
     total = void 0;
     soundcloud = require('../src/collector/soundcloud');
     return soundcloud.promise().then(function() {
-      return soundcloud.get('/users/nu-minor.json');
+      return soundcloud.get('/users/nu-minor/tracks.json');
     }).then(function(res) {
-      return total = res.json.track_count;
-    }).then(function() {
-      return count({
-        nm_type: 'soundcloud'
-      });
-    }).then(function(actual) {
-      console.log('total:', total, 'actual:', actual);
-      assert.equal(total, actual);
-      return done();
-    }).done();
+      var items = res.json;
+      assert(_.isArray(items));
+      return items;
+    }).then(function(items) {
+      return items.reduce(function(p, item) {
+        return p.then(function() {
+          return count({id: item.id});
+        }).then(function(count) {
+          console.log(count);
+          assert.equal(count, 1);
+        })
+      }, Q());
+    }).then(done);
   });
 });
 
