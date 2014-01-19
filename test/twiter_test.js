b0da710dd0366df90cedff628565a66f179728e0
piglovesyou <thepiglovesyou@gmail.com>
2014-01-19 13:22:51 +0900

    refac

--- test/twiter_test.js
+++ test/twiter_test.js
@@ -1,40 +1,29 @@
-(function() {
-  var Q, assert, count, db, find, http, youtube, _;
+var Q = require('q');
+var youtube = require('youtube-feeds');
+var assert = require('assert');
+var _ = require('underscore');
+var db = require('../src/setupdb');
+var http = require('../src/denodeify/http');
+var find = Q.denodeify(db.item.find.bind(db.item));
+var count = Q.denodeify(db.item.count.bind(db.item));
 
-  Q = require("q");
-
-  youtube = require("youtube-feeds");
-
-  assert = require("assert");
-
-  _ = require("underscore");
-
-  db = require("../src/setupdb");
-
-  http = require("../src/denodeify/http");
-
-  find = Q.denodeify(db.item.find.bind(db.item));
-
-  count = Q.denodeify(db.item.count.bind(db.item));
-
-  describe("TwitterCollector", function() {
-    return it("should have some data in DB.", function(done) {
-      var actualCount, twitter;
-      actualCount = 0;
-      twitter = require("../src/collector/twitter");
-      return twitter.promise().then(function() {
-        return twitter.get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=NU_minor");
-      }).then(function(items) {
-        actualCount = items.length;
-        return find({
-          nm_type: "twitter"
-        });
-      }).then(function(dbItems) {
-        assert(_.isArray(dbItems));
-        assert.equal(dbItems.length, actualCount);
-        return done();
-      }).done();
-    });
+describe('TwitterCollector', function() {
+  return it('should have some data in DB.', function(done) {
+    var actualCount, twitter;
+    actualCount = 0;
+    twitter = require('../src/collector/twitter');
+    return twitter.promise().then(function() {
+      return twitter.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=NU_minor');
+    }).then(function(items) {
+      actualCount = items.length;
+      return find({
+        nm_type: 'twitter'
+      });
+    }).then(function(dbItems) {
+      assert(_.isArray(dbItems));
+      assert.equal(dbItems.length, actualCount);
+      return done();
+    }).done();
   });
+});
 
-}).call(this);
